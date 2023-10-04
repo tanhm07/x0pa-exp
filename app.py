52108bbb5510e99c22915e37f86c35388149fb1a
tanhm07 <tanhm07@hotmail.com>
2023-10-04 22:55:44 +0800

    multiple uploads and scoring

--- app.py
+++ app.py
@@ -1,27 +1,13 @@
-
 import streamlit as st
 import PyPDF2
 from io import BytesIO
 
 def extract_text_from_pdf(pdf_file):
-    # Use PyPDF2's PdfReader to extract text from the uploaded PDF.
     pdf_reader = PyPDF2.PdfReader(pdf_file)
     text = ""
     for page in pdf_reader.pages:
         text += page.extract_text()
     return text
-
-st.title("CV Scoring App")
-
-uploaded_file = st.file_uploader("Upload your CV in PDF format", type="pdf")
-
-if uploaded_file:
-    # Convert the uploaded file to a BytesIO object to be processed by PyPDF2.
-    pdf_file = BytesIO(uploaded_file.read())
-    
-    extracted_text = extract_text_from_pdf(pdf_file)
-    st.write("Extracted Text from CV:")
-    st.write(extracted_text)
 
 def score_cv_against_job_description(cv_text, job_description):
     cv_keywords = set(cv_text.lower().split())
@@ -30,7 +16,23 @@ def score_cv_against_job_description(cv_text, job_description):
     score = len(overlap) / len(job_keywords)
     return score
 
+st.title("CV Scoring App")
+
+uploaded_files = st.file_uploader("Upload CVs in PDF format", type="pdf", accept_multiple_files=True)
+
 job_description = st.text_area("Enter the job description")
-if uploaded_file and job_description:
-    score = score_cv_against_job_description(extracted_text, job_description)
-    st.write(f"CV Score: {score:.2f}")
+
+if uploaded_files and job_description:
+    scores = {}
+    for file in uploaded_files:
+        pdf_file = BytesIO(file.read())
+        extracted_text = extract_text_from_pdf(pdf_file)
+        file_score = score_cv_against_job_description(extracted_text, job_description)
+        scores[file.name] = file_score
+
+    # Sort scores in descending order
+    sorted_scores = sorted(scores.items(), key=lambda x: x[1], reverse=True)
+    
+    for filename, score in sorted_scores:
+        st.write(f"CV: {filename} | Score: {score:.2f}")
+
